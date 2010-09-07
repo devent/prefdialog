e648d3026e5b60039411d5ea8e70ec5fb9eaf05c
Erwin Mueller <erwin.mueller@deventm.org>
2010-09-07 11:01:25 +0200

    Add test for reflection toolbox

--- prefdialog-default/src/main/java/com/globalscalingsoftware/prefdialog/internal/ReflectionToolbox.java
+++ prefdialog-default/src/main/java/com/globalscalingsoftware/prefdialog/internal/ReflectionToolbox.java
@@ -2,11 +2,21 @@ package com.globalscalingsoftware.prefdialog.internal;
 
 import static org.fest.reflect.core.Reflection.method;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
+import com.globalscalingsoftware.prefdialog.IAnnotationFilter;
 import com.globalscalingsoftware.prefdialog.IReflectionToolbox;
+import com.google.inject.Inject;
 
 public class ReflectionToolbox implements IReflectionToolbox {
+
+	private final IAnnotationFilter annotationFilter;
+
+	@Inject
+	ReflectionToolbox(IAnnotationFilter annotationFilter) {
+		this.annotationFilter = annotationFilter;
+	}
 
 	@Override
 	public Object getValueFrom(Field field, Object object) {
@@ -20,17 +30,49 @@ public class ReflectionToolbox implements IReflectionToolbox {
 		return name;
 	}
 
+	@Override
+	public void setValueTo(Field field, Object object, Object value) {
+		String name = getSetterName(field);
+		invokeMethodWithParameters(name, field.getType(), object, value);
+	}
+
 	private String getSetterName(Field field) {
 		String name = field.getName().substring(0, 1).toUpperCase();
 		name = "set" + name + field.getName().substring(1);
 		return name;
 	}
 
-	@Override
-	public void setValueTo(Field field, Object object, Object value) {
-		String name = getSetterName(field);
-		method(name).withParameterTypes(field.getType()).in(object)
-				.invoke(value);
+	private void invokeMethodWithParameters(String name,
+			Class<?> parameterType, Object object, Object value) {
+		method(name).withParameterTypes(parameterType).in(object).invoke(value);
 	}
 
+	@Override
+	public String getHelpText(Field field) {
+		for (Annotation a : field.getAnnotations()) {
+			if (annotationFilter.accept(a)) {
+				if (annotationHaveMethod(a, "helpText")) {
+					return invokeMethodWithReturnType("helpText", String.class,
+							a);
+				}
+			}
+		}
+		return "";
+	}
+
+	private <T> T invokeMethodWithReturnType(String methodName,
+			Class<? extends T> returnType, Object object) {
+		return method(methodName).withReturnType(returnType).in(object)
+				.invoke();
+	}
+
+	private boolean annotationHaveMethod(Annotation a, String methodName) {
+		try {
+			return a.annotationType().getMethod(methodName) != null;
+		} catch (SecurityException e) {
+			return false;
+		} catch (NoSuchMethodException e) {
+			return false;
+		}
+	}
 }
---
+++ prefdialog-default/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/ReflectionToolboxTest.groovy
@@ -0,0 +1,50 @@
+package com.globalscalingsoftware.prefdialog.internal
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+class ReflectionToolboxTest {
+	
+	def parentObject
+	
+	def property
+	
+	def propertyField
+	
+	def testValue
+	
+	def propertyHelpText
+	
+	@Before
+	void beforeTest() {
+		parentObject = new General()
+		property = parentObject.fields
+		propertyField = General.getDeclaredField("fields")
+		testValue = 99
+		propertyHelpText = "Can be 2 or more."
+	}
+	
+	@Test
+	void testGetValueFromProperty() {
+		def toolbox = new ReflectionToolbox(null)
+		def value = toolbox.getValueFrom(propertyField, parentObject)
+		assertThat value, is(property)
+	}
+	
+	@Test
+	void testSetValueToProperty() {
+		def toolbox = new ReflectionToolbox(null)
+		toolbox.setValueTo(propertyField, parentObject, testValue)
+		assertThat parentObject.fields, is(testValue)
+	}
+	
+	@Test
+	void testGetHelpTextFromProperty() {
+		def filter = new AnnotationsFilter()
+		def toolbox = new ReflectionToolbox(filter)
+		def helpText = toolbox.getHelpText(propertyField)
+		assertThat helpText, is(propertyHelpText)
+	}
+}
