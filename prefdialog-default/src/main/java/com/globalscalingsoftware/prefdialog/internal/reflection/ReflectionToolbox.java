cee795632f9aed15133382c8d8197ccb29edbd86
Erwin Mueller <erwin.mueller@deventm.org>
2010-09-29 16:07:40 +0200

    Document getValueFrom() Method

--- prefdialog-default/src/main/java/com/globalscalingsoftware/prefdialog/internal/reflection/ReflectionToolbox.java
+++ prefdialog-default/src/main/java/com/globalscalingsoftware/prefdialog/internal/reflection/ReflectionToolbox.java
@@ -15,17 +15,44 @@ import com.globalscalingsoftware.prefdialog.Validator;
 @Stateless
 public class ReflectionToolbox {
 
+	/**
+	 * Return the value from a field.
+	 * 
+	 * If a getter for this field is defined it tries to use the getter first. A
+	 * getter is a method with the pattern <code>FieldType getFieldName()</code>
+	 * where <code>FieldType</code> the type of the field and
+	 * <code>FieldName</code> the name of the field is.
+	 * 
+	 * If no such getter is defined, the value of the field will be returned by
+	 * using reflection.
+	 * 
+	 * @param field
+	 *            the {@link Field} that provides access to the field of the
+	 *            class.
+	 * @param object
+	 *            the {@link Object} of the field.
+	 * @return the {@link Object value} of the field.
+	 * @throws ReflectionError
+	 *             if there was no getter method and it was an illegal access to
+	 *             the field thought reflection.
+	 */
 	public Object getValueFrom(Field field, Object object) {
-		String name = getGetterName(field);
 		try {
-			return getValueFromGetter(object, name);
+			return getValueFromGetter(field, object);
 		} catch (ReflectionError e) {
 			return getValueFromField(field, object);
 		}
 	}
 
-	private Object getValueFromGetter(Object object, String name) {
+	private Object getValueFromGetter(Field field, Object object) {
+		String name = getGetterName(field);
 		return method(name).in(object).invoke();
+	}
+
+	private String getGetterName(Field field) {
+		String name = field.getName().substring(0, 1).toUpperCase();
+		name = "get" + name + field.getName().substring(1);
+		return name;
 	}
 
 	private Object getValueFromField(Field field, Object object) {
@@ -36,12 +63,6 @@ public class ReflectionToolbox {
 					"Illegal access to field '%s' in object '%s'.", field,
 					object), e);
 		}
-	}
-
-	private String getGetterName(Field field) {
-		String name = field.getName().substring(0, 1).toUpperCase();
-		name = "get" + name + field.getName().substring(1);
-		return name;
 	}
 
 	public void setValueTo(Field field, Object object, Object value) {
