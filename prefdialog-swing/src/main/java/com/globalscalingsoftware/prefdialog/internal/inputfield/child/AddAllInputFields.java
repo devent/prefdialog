b723bbafb13417fc1fdfcce2a1df343def742b1a
Erwin Mueller <erwin.mueller@deventm.org>
2010-11-01 16:44:04 +0100

    Sort out static and transient fields

--- prefdialog-swing/src/main/java/com/globalscalingsoftware/prefdialog/internal/inputfield/child/AddAllInputFields.java
+++ prefdialog-swing/src/main/java/com/globalscalingsoftware/prefdialog/internal/inputfield/child/AddAllInputFields.java
@@ -1,6 +1,9 @@
 package com.globalscalingsoftware.prefdialog.internal.inputfield.child;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.fest.reflect.exception.ReflectionError;
@@ -28,11 +31,39 @@ class AddAllInputFields {
 
 	private void addAllInputFields(Map<Field, FieldHandler<?>> inputFields,
 			Object parentObject) {
-		for (Field field : parentObject.getClass().getDeclaredFields()) {
+		List<Field> fields = getPreferenceFields(parentObject);
+		for (Field field : fields) {
 			FieldHandler<?> inputField = createInputField(parentObject,
 					reflectionToolbox, field);
 			addInputField(inputFields, field, inputField);
 		}
+	}
+
+	private List<Field> getPreferenceFields(Object parentObject) {
+		Field[] declaredFields = parentObject.getClass().getDeclaredFields();
+		List<Field> fields = new ArrayList<Field>(declaredFields.length);
+		addPreferenceFields(fields, declaredFields);
+		return fields;
+	}
+
+	private void addPreferenceFields(List<Field> fields, Field[] declaredFields) {
+		for (Field field : declaredFields) {
+			if (fieldIsStatic(field)) {
+				continue;
+			}
+			if (fieldIsTransient(field)) {
+				continue;
+			}
+			fields.add(field);
+		}
+	}
+
+	private boolean fieldIsTransient(Field field) {
+		return Modifier.isTransient(field.getModifiers());
+	}
+
+	private boolean fieldIsStatic(Field field) {
+		return Modifier.isStatic(field.getModifiers());
 	}
 
 	private FieldHandler<?> createInputField(Object parentObject,
