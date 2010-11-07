6e9ebc1c75db2c2a114eeccae81a52f5a2397bc1
Erwin Mueller <erwin.mueller@deventm.org>
2010-11-07 22:15:33 +0100

    #181: Refactor abstract preference test for FEST
    Make the real tests as minimal as possible, only need
    to setup the preferences and write the test.

--- prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/AbstractPreferenceTest.groovy
+++ prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/AbstractPreferenceTest.groovy
@@ -4,6 +4,11 @@ import java.lang.reflect.Field;
 
 import javax.swing.SwingUtilities 
 import javax.swing.UIManager;
+import org.fest.swing.edt.GuiActionRunner 
+import org.fest.swing.edt.GuiQuery 
+import org.fest.swing.fixture.FrameFixture 
+import org.junit.After;
+import org.junit.Before;
 
 import com.globalscalingsoftware.prefdialog.internal.reflection.ReflectionToolbox;
 
@@ -38,13 +43,37 @@ abstract class AbstractPreferenceTest {
 		}
 	}
 	
-	protected createDialog(def preferencesPanel) {
-		new SwingBuilder().build {
-			dialog(title:'Preferences Panel', size:[480, 640], modal: true, show: true, locationByPlatform: true) {
-				borderLayout()
-				widget(preferencesPanel())
-			}
-		}
+	protected preferencesClass
+	
+	protected preferences
+	
+	protected preferencesParentValue
+	
+	protected preferencesParentName
+	
+	protected FrameFixture window
+	
+	@Before
+	void beforeTest() {
+		setupPreferences()
+		window = createFrameFixture(preferencesClass, preferences, preferencesParentName, preferencesParentValue)
+		window.show();
+	}
+	
+	abstract setupPreferences()
+	
+	@After
+	void afterTest() {
+		window.cleanUp()
+	}
+	
+	private createFrameFixture(def preferencesClass, def preferences, def parentName, def parentValue) {
+		def injector = new PreferencesDialogInjectorFactory().create(preferences)
+		def field = getPreferencesField(preferencesClass, parentName)
+		def frame = GuiActionRunner.execute([executeInEDT: { 
+				return createFrame(injector, preferences, field, parentValue)
+			} ] as GuiQuery);
+		return new FrameFixture(frame);
 	}
 	
 	protected Field getPreferencesField(def preferencesClass, def fieldName) {
@@ -57,7 +86,7 @@ abstract class AbstractPreferenceTest {
 		return field
 	}
 	
-	protected createField(def injector, def preferences, def field, def parentValue) {
+	protected createFrame(def injector, def preferences, def field, def parentValue) {
 		def factory = injector.getInstance(FieldsFactory)
 		def reflectionToolbox = injector.getInstance(ReflectionToolbox)
 		def inputfield = factory.createField(preferences, field, parentValue)
@@ -66,24 +95,11 @@ abstract class AbstractPreferenceTest {
 		inputfield.setFieldsFactory(factory)
 		inputfield.setup()
 		
-		createDialog({ inputfield.getAWTComponent() })
-		return inputfield
+		return createFrame({ return inputfield.getAWTComponent() })
 	}
 	
-	protected createDialog(def injector, def preferences, def field, def parentValue) {
-		def factory = injector.getInstance(FieldsFactory)
-		def reflectionToolbox = injector.getInstance(ReflectionToolbox)
-		def inputfield = factory.createField(preferences, field, parentValue)
-		
-		inputfield.setReflectionToolbox(reflectionToolbox)
-		inputfield.setFieldsFactory(factory)
-		inputfield.setup()
-		
-		createDialog2({ inputfield.getAWTComponent() })
-	}
-	
-	protected createDialog2(def preferencesPanel) {
-		new SwingBuilder().build {
+	private createFrame(def preferencesPanel) {
+		return new SwingBuilder().build {
 			frame(title:'Preferences Panel', size:[480, 640], locationByPlatform: true) {
 				borderLayout()
 				widget(preferencesPanel())
--- prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/checkbox/CheckboxPreferedWidthPreferencePanelTest.groovy
+++ prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/checkbox/CheckboxPreferedWidthPreferencePanelTest.groovy
@@ -1,22 +1,12 @@
 package com.globalscalingsoftware.prefdialog.internal.checkbox
 
-import javax.swing.JFrame;
-
-
-
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
-import org.fest.swing.edt.GuiActionRunner;
-import org.fest.swing.edt.GuiQuery;
-import org.fest.swing.fixture.FrameFixture;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import com.globalscalingsoftware.prefdialog.annotations.fields.Checkbox;
 import com.globalscalingsoftware.prefdialog.annotations.fields.Child;
 import com.globalscalingsoftware.prefdialog.internal.AbstractPreferenceTest 
-import com.globalscalingsoftware.prefdialog.internal.PreferencesDialogInjectorFactory 
 
 class CheckboxPreferedWidthPreferencePanelTest extends AbstractPreferenceTest {
 	
@@ -37,35 +27,11 @@ class CheckboxPreferedWidthPreferencePanelTest extends AbstractPreferenceTest {
 		General general = new General()
 	}
 	
-	def preferences
-	
-	def parentValue
-	
-	def field
-	
-	def injector
-	
-	private FrameFixture window
-	
-	@Before
-	void beforeTest() {
+	def setupPreferences() {
+		preferencesClass = Preferences
 		preferences = new Preferences()
-		parentValue = preferences.general
-		field = getPreferencesField(Preferences, "general")
-		injector = new PreferencesDialogInjectorFactory().create(preferences)
-		
-		JFrame frame = GuiActionRunner.execute(new GuiQuery<JFrame>() {
-					protected JFrame executeInEDT() {
-						return createDialog(injector, preferences, field, parentValue)
-					}
-				});
-		window = new FrameFixture(frame);
-		window.show();
-	}
-	
-	@After
-	void afterTest() {
-		window.cleanUp()
+		preferencesParentName = "general"
+		preferencesParentValue = preferences.general
 	}
 	
 	@Test
