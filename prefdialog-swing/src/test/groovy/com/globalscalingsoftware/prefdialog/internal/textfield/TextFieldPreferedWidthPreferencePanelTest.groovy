d985e09637f432efacc73ea30e6e76a7fe0a631f
Erwin Mueller <erwin.mueller@deventm.org>
2010-11-08 18:18:37 +0100

    #181: Fixes Text Field test with preferred with

--- prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/textfield/TextFieldPreferedWidthPreferencePanelTest.groovy
+++ prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/textfield/TextFieldPreferedWidthPreferencePanelTest.groovy
@@ -4,13 +4,11 @@ package com.globalscalingsoftware.prefdialog.internal.textfield
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
-import org.junit.Before;
 import org.junit.Test;
 
 import com.globalscalingsoftware.prefdialog.annotations.fields.Child;
 import com.globalscalingsoftware.prefdialog.annotations.fields.TextField;
 import com.globalscalingsoftware.prefdialog.internal.AbstractPreferenceTest 
-import com.globalscalingsoftware.prefdialog.internal.PreferencesDialogInjectorFactory 
 
 class TextFieldPreferedWidthPreferencePanelTest extends AbstractPreferenceTest {
 	
@@ -31,25 +29,20 @@ class TextFieldPreferedWidthPreferencePanelTest extends AbstractPreferenceTest {
 		General general = new General()
 	}
 	
-	def preferences
 	
-	def parentValue
-	
-	def field
-	
-	def injector
-	
-	@Before
-	void beforeTest() {
+	def setupPreferences() {
+		preferencesClass = Preferences
 		preferences = new Preferences()
-		parentValue = preferences.general
-		field = getPreferencesField(Preferences, "general")
-		injector = new PreferencesDialogInjectorFactory().create(preferences)
+		preferencesParentName = "general"
+		preferencesParentValue = preferences.general
 	}
 	
 	@Test
 	void testPanelClickApplyAndClose() {
-		def filed = createField(injector, preferences, field, parentValue)
-		assertThat preferences.general.name, is("name")
+		window.textBox("name").enterText "test"
+		window.panel("general").button("apply").click()
+		
+		window.label("label-name").requireText "name: "
+		assert preferences.general.name == "test"
 	}
 }
