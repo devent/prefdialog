a098a703091f7ea277605a83745109dbc4463d20
Erwin Mueller <erwin.mueller@deventm.org>
2010-11-08 16:01:51 +0100

    #181: Automatic test for checkbox panel

--- prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/checkbox/CheckboxPreferencePanelTest.groovy
+++ prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/checkbox/CheckboxPreferencePanelTest.groovy
@@ -4,13 +4,11 @@ package com.globalscalingsoftware.prefdialog.internal.checkbox
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
-import org.junit.Before;
 import org.junit.Test;
 
 import com.globalscalingsoftware.prefdialog.annotations.fields.Checkbox;
 import com.globalscalingsoftware.prefdialog.annotations.fields.Child;
 import com.globalscalingsoftware.prefdialog.internal.AbstractPreferenceTest 
-import com.globalscalingsoftware.prefdialog.internal.PreferencesDialogInjectorFactory 
 
 class CheckboxPreferencePanelTest extends AbstractPreferenceTest {
 	
@@ -31,25 +29,16 @@ class CheckboxPreferencePanelTest extends AbstractPreferenceTest {
 		General general = new General()
 	}
 	
-	def preferences
-	
-	def parentValue
-	
-	def field
-	
-	def injector
-	
-	@Before
-	void beforeTest() {
+	def setupPreferences() {
+		preferencesClass = Preferences
 		preferences = new Preferences()
-		parentValue = preferences.general
-		field = getPreferencesField(Preferences, "general")
-		injector = new PreferencesDialogInjectorFactory().create(preferences)
+		preferencesParentName = "general"
+		preferencesParentValue = preferences.general
 	}
 	
 	@Test
 	void testPanelClickApplyAndClose() {
-		def filed = createField(injector, preferences, field, parentValue)
-		assertThat preferences.general.automaticSave, is(true)
+		window.checkBox("automaticSave").click()
+		window.panel("general").button("apply").click()
 	}
 }
