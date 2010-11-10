8ad2c38d08ff67a5acdc89b9c94fa793290917ba
Erwin Mueller <erwin.mueller@deventm.org>
2010-11-10 23:50:42 +0100

    #185: Add Reflection Toolbox Tests

---
+++ prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/reflection/AbstractReflectionToolboxTest.groovy
@@ -0,0 +1,53 @@
+package com.globalscalingsoftware.prefdialog.internal.reflection
+
+import com.globalscalingsoftware.prefdialog.annotations.fields.TextField 
+import com.globalscalingsoftware.prefdialog.validators.NotEmptyString 
+import org.junit.Before;
+
+class AbstractReflectionToolboxTest {
+	
+	static final PROPERTY_VALUE = "property_value"
+	
+	static final HELP_TEXT = "help text"
+	
+	static class Bean {
+		
+		@TextField(validator=NotEmptyString, validatorText=AbstractReflectionToolboxTest.HELP_TEXT)
+		String property = AbstractReflectionToolboxTest.PROPERTY_VALUE
+	}
+	
+	static class ParentBean {
+		
+		def bean = new Bean()
+		
+		def someMethod(String someParameter) {
+		}
+		
+		def someMethodWithReturn() {
+			return "test"
+		}
+	}
+	
+	def parentObject
+	
+	def bean
+	
+	def property
+	
+	def propertyField
+	
+	def testValue
+	
+	def propertyHelpText
+	
+	ReflectionToolbox toolbox
+	
+	@Before
+	void beforeTest() {
+		parentObject = new ParentBean()
+		bean = parentObject.bean
+		property = parentObject.bean.property
+		propertyField = Bean.getDeclaredField("property")
+		toolbox = new ReflectionToolbox()
+	}
+}
---
+++ prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/reflection/ReflectionToolboxTest.groovy
@@ -0,0 +1,40 @@
+package com.globalscalingsoftware.prefdialog.internal.reflection
+
+import org.junit.Test;
+
+import com.globalscalingsoftware.prefdialog.internal.reflection.ReflectionToolbox;
+
+class ReflectionToolboxTest extends AbstractReflectionToolboxTest {
+	
+	@Test
+	void testGetValueFromProperty() {
+		def value = toolbox.getValueFrom(propertyField, bean)
+		assert value == PROPERTY_VALUE
+	}
+	
+	@Test
+	void testSetValueToProperty() {
+		def newValue = "new_value"
+		toolbox.setValueTo(propertyField, bean, newValue)
+		assert bean.property == newValue
+	}
+	
+	@Test
+	void testInvokeMethodWithParameters() {
+		def name = "someMethod"
+		def parameterType = String
+		def object = parentObject
+		def value = "some string"
+		toolbox.invokeMethodWithParameters name, parameterType, object, value
+	}
+	
+	@Test
+	void testInvokeMethodWithReturnType() {
+		def name = "someMethodWithReturn"
+		def returnType = String
+		def object = parentObject
+		
+		def value = toolbox.invokeMethodWithReturnType(name, returnType, object)
+		assert value == "test"
+	}
+}
