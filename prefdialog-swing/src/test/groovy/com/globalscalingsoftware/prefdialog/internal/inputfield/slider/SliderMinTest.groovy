102dd09177b036582b91ccc765ca551350e7ed7f
Erwin Mueller <erwin.mueller@deventm.org>
2010-11-24 14:31:30 +0100

    #196: Add Slider min/max tests

---
+++ prefdialog-swing/src/test/groovy/com/globalscalingsoftware/prefdialog/internal/inputfield/slider/SliderMinTest.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of prefdialog-swing.
+ * 
+ * prefdialog-swing is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * prefdialog-swing is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with prefdialog-swing. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.globalscalingsoftware.prefdialog.internal.inputfield.slider
+
+
+import org.junit.Test;
+
+import com.globalscalingsoftware.prefdialog.annotations.fields.Child;
+import com.globalscalingsoftware.prefdialog.annotations.fields.Slider 
+import com.globalscalingsoftware.prefdialog.internal.AbstractPreferencePanelTest 
+
+class SliderMinTest extends AbstractPreferencePanelTest {
+	
+	static class General {
+		
+		@Slider(min=-50)
+		int slider = 50
+		
+		@Override
+		public String toString() {
+			"General"
+		}
+	}
+	
+	static class Preferences {
+		
+		@Child
+		General general = new General()
+	}
+	
+	def setupPreferences() {
+		preferencesClass = Preferences
+		preferences = new Preferences()
+		preferencesParentName = "general"
+		preferencesParentValue = preferences.general
+	}
+	
+	@Test
+	void testPanelClickApplyAndClose() {
+		window.slider("slider").slideToMinimum()
+		window.panel("general").button("apply").click()
+		
+		assert preferences.general.slider == -50
+	}
+}
