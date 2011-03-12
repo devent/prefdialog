e0fb884fee7730eae572fd7dfda6ceaa51cff7ec
Erwin Mueller <erwin.mueller@deventm.org>
2011-03-12 13:05:29 +0100

    #344: Rename to preference dialog controller factory

--- prefdialog-public/src/main/java/com/globalscalingsoftware/prefdialog/PreferenceDialogControllerFactory.java
+++ prefdialog-public/src/main/java/com/globalscalingsoftware/prefdialog/PreferenceDialogControllerFactory.java
@@ -4,7 +4,7 @@ import java.awt.Frame;
 
 import com.google.inject.assistedinject.Assisted;
 
-public interface PreferenceDialogFactory {
+public interface PreferenceDialogControllerFactory {
 
 	PreferenceDialogController create(@Assisted Frame owner,
 			@Assisted Object preferences);
