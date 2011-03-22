9a6371c9c1ea785652628e6329c619f42e03fae5
Erwin Mueller <erwin.mueller@deventm.org>
2011-03-22 10:04:39 +0100

    #404: Make the Child Selected Callback private

--- prefdialog-swing/src/main/java/com/globalscalingsoftware/prefdialog/dialog/internal/ChildSelectedCallback.java
+++ prefdialog-swing/src/main/java/com/globalscalingsoftware/prefdialog/dialog/internal/ChildSelectedCallback.java
@@ -1,7 +1,16 @@
 package com.globalscalingsoftware.prefdialog.dialog.internal;
 
-public interface ChildSelectedCallback {
+/**
+ * Callback that is called if the user selects a new child panel.
+ */
+interface ChildSelectedCallback {
 
+	/**
+	 * Called if the user selects a new child panel.
+	 * 
+	 * @param value
+	 *            the preference value that the child panel represents.
+	 */
 	void call(Object value);
 
 }
