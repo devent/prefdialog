ab9a9f5e5b7fc8d0509e5033de3400223535a466
Erwin Mueller <erwin.mueller@deventm.org>
2011-08-10 22:17:39 +0800

    internal package: fix reflection tool box test

--- prefdialog-reflection/src/test/groovy/com/globalscalingsoftware/prefdialog/reflection/AbstractReflectionToolbox.groovy
+++ prefdialog-reflection/src/test/groovy/com/globalscalingsoftware/prefdialog/reflection/AbstractReflectionToolbox.groovy
@@ -18,49 +18,49 @@
  */
 package com.globalscalingsoftware.prefdialog.reflection
 
-import com.globalscalingsoftware.prefdialog.annotations.TextField 
-import com.globalscalingsoftware.prefdialog.reflection.internal.ReflectionToolbox 
-import com.globalscalingsoftware.prefdialog.validators.NotEmptyString 
-import org.junit.Before;
+import org.junit.Before
+
+import com.globalscalingsoftware.prefdialog.annotations.TextField
+import com.globalscalingsoftware.prefdialog.validators.NotEmptyString
 
 class AbstractReflectionToolbox {
-	
+
 	static final PROPERTY_VALUE = "property_value"
-	
+
 	static final HELP_TEXT = "help text"
-	
+
 	static class Bean {
-		
+
 		@TextField(validator=NotEmptyString, validatorText=AbstractReflectionToolbox.HELP_TEXT)
 		String property = AbstractReflectionToolbox.PROPERTY_VALUE
 	}
-	
+
 	static class ParentBean {
-		
+
 		def bean = new Bean()
-		
+
 		def someMethod(String someParameter) {
 		}
-		
+
 		def someMethodWithReturn() {
 			return "test"
 		}
 	}
-	
+
 	def parentObject
-	
+
 	def bean
-	
+
 	def property
-	
+
 	def propertyField
-	
+
 	def testValue
-	
+
 	def propertyHelpText
-	
+
 	ReflectionToolbox toolbox
-	
+
 	@Before
 	void beforeTest() {
 		parentObject = new ParentBean()
