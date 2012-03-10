68fc545152bf6e8ea817af2f208126e7ca759038
Erwin Mueller <erwin.mueller@deventm.org>
2012-03-10 01:24:49 +0100

    Add test for custom text
    #1127

---
+++ prefdialog-corefields/src/test/groovy/com/anrisoftware/prefdialog/panel/inputfields/filechooser/FileChooserTextTest.groovy
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2010 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of prefdialog-swing.
+ * 
+ * prefdialog-swing is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * prefdialog-swing is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with prefdialog-swing. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.prefdialog.panel.inputfields.filechooser
+
+import org.junit.Test
+
+import com.anrisoftware.prefdialog.annotations.FileChooser
+import com.anrisoftware.prefdialog.panel.inputfields.FieldFixtureHandler
+import com.anrisoftware.prefdialog.panel.inputfields.api.FileChooserFieldHandlerFactory
+
+class FileChooserTextTest extends FieldFixtureHandler {
+
+	static factory = injector.getInstance(FileChooserFieldHandlerFactory)
+
+	static final String FILE_CUSTOM_TEXT = "fileCustomText"
+
+	static final String FILE_DEFAULT = "fileDefault"
+
+	static final String DEFAULT_TEXT = "Open…"
+
+	static final String CUSTOM_TEXT = "custom"
+
+	static class General {
+
+		@FileChooser
+		File fileDefault = new File("")
+
+		@FileChooser(text="custom")
+		File fileCustomText = new File("")
+	}
+
+	@Test
+	void "default text"() {
+		createFieldFixture(new General(), FILE_DEFAULT, factory)
+		beginFixture()
+		fixture.button("openfilebutton-$FILE_DEFAULT").requireText(DEFAULT_TEXT)
+		endFixture()
+	}
+
+	@Test
+	void "custom text"() {
+		createFieldFixture(new General(), FILE_CUSTOM_TEXT, factory)
+		beginFixture()
+		fixture.button("openfilebutton-$FILE_CUSTOM_TEXT").requireText(CUSTOM_TEXT)
+		endFixture()
+	}
+
+	@Test
+	void testManual() {
+		createFieldFixture(new General(), FILE_DEFAULT, factory)
+		beginFixture()
+		Thread.sleep 5000
+		endFixture()
+	}
+}
