fd2baa711c1ca9ce6d4a84e9bec41729174848fb
Erwin Mueller <erwin.mueller@deventm.org>
2012-03-15 03:02:32 +0100

    Create convenient method to run the frame fixture tests
    #1129

--- prefdialog-corefields/src/test/groovy/com/anrisoftware/prefdialog/panel/inputfields/AbstractFixtureHandler.groovy
+++ prefdialog-corefields/src/test/groovy/com/anrisoftware/prefdialog/panel/inputfields/AbstractFixtureHandler.groovy
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of prefdialog-corefields.
+ * 
+ * prefdialog-corefields is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * prefdialog-corefields is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with prefdialog-corefields. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anrisoftware.prefdialog.panel.inputfields
 
 import groovy.swing.SwingBuilder
@@ -11,7 +29,7 @@ import org.fest.swing.edt.GuiQuery
 import org.fest.swing.fixture.FrameFixture
 
 /**
- * Create a frame fixture for a {@link Component}.
+ * Create a frame fixture for a {@link Component} field.
  * 
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 2.1
@@ -22,7 +40,7 @@ class AbstractFixtureHandler {
 
 	JFrame frame
 
-	def createPanelFrame(def component) {
+	def createPanelFrame(def component, def closure) {
 		return new SwingBuilder().frame(title: 'Core Field Test', pack: true, preferredSize: [480, 360]) {
 			borderLayout()
 			widget(component, constraints: BorderLayout.CENTER)
--- prefdialog-corefields/src/test/groovy/com/anrisoftware/prefdialog/panel/inputfields/FieldFixtureHandler.groovy
+++ prefdialog-corefields/src/test/groovy/com/anrisoftware/prefdialog/panel/inputfields/FieldFixtureHandler.groovy
@@ -1,23 +1,22 @@
 /*
- * Copyright 2010 Erwin Müller <erwin.mueller@deventm.org>
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
  * 
- * This file is part of prefdialog-swing.
+ * This file is part of prefdialog-corefields.
  * 
- * prefdialog-swing is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
+ * prefdialog-corefields is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  * 
- * prefdialog-swing is distributed in the hope that it will be useful, but
+ * prefdialog-corefields is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with prefdialog-swing. If not, see <http://www.gnu.org/licenses/>.
+ * along with prefdialog-corefields. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.prefdialog.panel.inputfields
-
 
 import groovy.swing.SwingBuilder
 
@@ -36,6 +35,12 @@ import com.anrisoftware.prefdialog.FieldHandlerFactory
 import com.google.inject.Guice
 import com.google.inject.Injector
 
+/**
+ * Created a {@link FrameFixture} for a field.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 2.1
+ */
 abstract class FieldFixtureHandler {
 
 	public static Injector injector = Guice.createInjector(new PrefdialogCoreFieldsModule())
@@ -58,7 +63,30 @@ abstract class FieldFixtureHandler {
 
 	JFrame frame
 
-	def createFieldFixture(def parentObject, def fieldName, FieldHandlerFactory fieldFactory, def lookAndFeel="javax.swing.plaf.metal.MetalLookAndFeel") {
+	/**
+	 * Begin the {@link FrameFixture} for the field, run the test and end the 
+	 * fixture.
+	 * 
+	 * @param parentObject
+	 * 		the properties object.
+	 * 
+	 * @param fieldName
+	 * 		the name of the field in the properties object.
+	 * 
+	 * @param fieldFactory
+	 * 		the {@link FieldHandlerFactory} to create the new field handler.
+	 * 
+	 * @param run
+	 * 		a {@link Closure} to run the test.
+	 */
+	void runFieldFixture(def parentObject, def fieldName, FieldHandlerFactory fieldFactory, def run, def lookAndFeel="javax.swing.plaf.metal.MetalLookAndFeel") {
+		createFieldFixture(parentObject, fieldName, fieldFactory, lookAndFeel)
+		beginFixture()
+		run()
+		endFixture()
+	}
+
+	void createFieldFixture(def parentObject, def fieldName, FieldHandlerFactory fieldFactory, def lookAndFeel="javax.swing.plaf.metal.MetalLookAndFeel") {
 		UIManager.setLookAndFeel(lookAndFeel)
 		this.parentObject = parentObject
 		this.value = parentObject."$fieldName"
