adfb910a340a7ba7dbf8d2411c8ffcaaef5576f2
Erwin Mueller <erwin.mueller@deventm.org>
2012-05-26 17:05:22 +0800

    Select different child panels in the dialog

--- prefdialog-dialog/src/test/groovy/com/anrisoftware/prefdialog/dialog/PreferencesDialogTest.groovy
+++ prefdialog-dialog/src/test/groovy/com/anrisoftware/prefdialog/dialog/PreferencesDialogTest.groovy
@@ -12,9 +12,7 @@ import org.junit.Before
 import org.junit.Test
 
 import com.anrisoftware.globalpom.utils.TestFrameUtil
-import com.anrisoftware.prefdialog.ChildrenListPanel
 import com.anrisoftware.prefdialog.ChildrenListPanelFactory
-import com.anrisoftware.prefdialog.ChildrenPanel
 import com.anrisoftware.prefdialog.ChildrenPanelFactory
 import com.anrisoftware.prefdialog.PreferenceDialogFactory;
 import com.anrisoftware.prefdialog.panel.inputfields.PrefdialogCoreFieldsModule
@@ -66,34 +64,12 @@ class PreferencesDialogTest extends TestFrameUtil {
 			dialog.visible = true
 			fixture.dialog("$name-$DIALOG_NAME_POSTFIX").requireVisible()
 
-			Thread.sleep 5000
-		}
-	}
-
-	@Test
-	void "add child nodes"() {
-		def name = "test"
-
-		def childrenListPanel = childrenListPanelFactory.create new JPanel()
-		childrenListPanel.addChildNode(new DefaultMutableTreeNode("Aaa"))
-		childrenListPanel.addChildNode(new DefaultMutableTreeNode("Bbb"))
-		childrenListPanel.addChildNode(new DefaultMutableTreeNode("Ccc"))
-
-		def childrenPanel = childrenPanelFactory.create new JPanel(), childrenListPanel
-		def preferencesDialog = factory.create dialog, childrenPanel
-
-		beginPanelFrame TITLE, null, {
-			frame.visible = false
-			preferencesDialog.name = name
-			dialog.visible = true
-			dialog.name = name
-			Thread.sleep 60000
+			Thread.sleep 2000
 		}
 	}
 
 	@Test
 	void "set different child panels"() {
-		def panel = new JPanel()
 		def name = "test"
 		def childs = ["Aaa", "Bbb", "Ccc"]
 		def panels = [
@@ -105,23 +81,29 @@ class PreferencesDialogTest extends TestFrameUtil {
 		panels[1].add new JLabel("Bbb")
 		panels[2].add new JLabel("Ccc")
 
-		ChildrenListPanel childrenListPanel = childrenListPanelFactory.create new JPanel()
+		def childrenListPanel = childrenListPanelFactory.create new JPanel()
 		childrenListPanel.addChildNode(new DefaultMutableTreeNode(childs[0]))
 		childrenListPanel.addChildNode(new DefaultMutableTreeNode(childs[1]))
 		childrenListPanel.addChildNode(new DefaultMutableTreeNode(childs[2]))
 
-		ChildrenPanel childrenPanel = childrenPanelFactory.create panel, childrenListPanel
+		def childrenPanel = childrenPanelFactory.create new JPanel(), childrenListPanel
+		def preferencesDialog = factory.create dialog, childrenPanel
 
-		beginPanelFrame TITLE, panel, {
-			childrenPanel.name = name
+		beginPanelFrame TITLE, null, {
+			preferencesDialog.name = name
+			dialog.visible = true
+			fixture.dialog("$name-$DIALOG_NAME_POSTFIX").requireVisible()
 			assert childrenListPanel.selectedChild == null
 			Thread.sleep 2000
+
 			childrenListPanel.setSelectedChild childs[0]
 			childrenPanel.childPanel = panels[0]
 			Thread.sleep 500
+
 			childrenListPanel.setSelectedChild childs[1]
 			childrenPanel.childPanel = panels[1]
 			Thread.sleep 500
+
 			childrenListPanel.setSelectedChild childs[2]
 			childrenPanel.childPanel = panels[2]
 			Thread.sleep 1000
