ad874b58d2b9ba290fae24ff37db92c043fb0746
Erwin Mueller <erwin.mueller@deventm.org>
2012-05-28 18:28:34 +0800

    For the children tab panel we need to save the created JPanels
    Because we need to get the index of the JPanel in the JTabbedPane.

--- prefdialog-dialog/src/main/java/com/anrisoftware/prefdialog/dialog/childrenpanels/EmptyChildrenPanels.java
+++ prefdialog-dialog/src/main/java/com/anrisoftware/prefdialog/dialog/childrenpanels/EmptyChildrenPanels.java
@@ -3,12 +3,14 @@ package com.anrisoftware.prefdialog.dialog.childrenpanels;
 import static java.awt.BorderLayout.CENTER;
 
 import java.awt.BorderLayout;
+import java.util.HashMap;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.anrisoftware.prefdialog.ChildrenPanel;
 import com.anrisoftware.prefdialog.ChildrenPanels;
+import com.google.common.collect.Maps;
 
 /**
  * Returns a panel with a label that shows the selected child. Is used as a
@@ -19,11 +21,26 @@ import com.anrisoftware.prefdialog.ChildrenPanels;
  */
 public class EmptyChildrenPanels implements ChildrenPanels {
 
+	private final HashMap<Object, JPanel> panels;
+
+	EmptyChildrenPanels() {
+		panels = Maps.newHashMap();
+	}
+
 	@Override
 	public JPanel getChildPanel(ChildrenPanel panel, Object child) {
-		JPanel childPanel = new JPanel(new BorderLayout());
-		JLabel label = new JLabel(child.toString());
-		childPanel.add(label, CENTER);
+		JPanel childPanel = panels.get(child);
+		childPanel = lazyCreateChildPanel(child, childPanel);
+		return childPanel;
+	}
+
+	private JPanel lazyCreateChildPanel(Object child, JPanel childPanel) {
+		if (childPanel == null) {
+			childPanel = new JPanel(new BorderLayout());
+			JLabel label = new JLabel(child.toString());
+			childPanel.add(label, CENTER);
+			panels.put(child, childPanel);
+		}
 		return childPanel;
 	}
 
