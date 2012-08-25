b6baf764514b972104dd0e4a9f32594c8dbd614b
Erwin Mueller <erwin.mueller@deventm.org>
2012-08-25 04:48:01 +0200

    Add license text for annotation access

--- prefdialog-reflection/src/test/groovy/com/anrisoftware/prefdialog/reflection/annotations/AnnotationUtils.groovy
+++ prefdialog-reflection/src/test/groovy/com/anrisoftware/prefdialog/reflection/annotations/AnnotationUtils.groovy
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of prefdialog-reflection.
+ * 
+ * prefdialog-reflection is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * prefdialog-reflection is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with prefdialog-reflection. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anrisoftware.prefdialog.reflection.annotations
 
 import org.junit.Before
 
 import com.anrisoftware.globalpom.utils.TestUtils
-import com.anrisoftware.prefdialog.reflection.utils.ParentBean;
+import com.anrisoftware.prefdialog.reflection.utils.ParentBean
 import com.google.inject.Guice
 import com.google.inject.Injector
 
