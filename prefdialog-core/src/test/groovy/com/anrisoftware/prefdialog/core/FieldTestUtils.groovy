0fda30f2950cfcb0eb2095675eeb23795180d7ce
Erwin Mueller <erwin.mueller@deventm.org>
2012-08-26 11:08:54 +0200

    Add missing javadoc and LGPL license text

--- prefdialog-core/src/test/groovy/com/anrisoftware/prefdialog/core/FieldTestUtils.groovy
+++ prefdialog-core/src/test/groovy/com/anrisoftware/prefdialog/core/FieldTestUtils.groovy
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of prefdialog-core.
+ * 
+ * prefdialog-core is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * prefdialog-core is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with prefdialog-core. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anrisoftware.prefdialog.core
 
 import org.junit.Before
@@ -20,6 +38,12 @@ import com.anrisoftware.resources.texts.maps.TextsDefaultMapsModule
 import com.google.inject.Guice
 import com.google.inject.Injector
 
+/**
+ * Creates the injector and the field component factory; Create the texts and images resource.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
 class FieldTestUtils extends TestFrameUtil {
 
 	Injector injector
@@ -30,7 +54,6 @@ class FieldTestUtils extends TestFrameUtil {
 
 	@Before
 	void beforeTest() {
-
 		injector = createInjector()
 		fieldComponentFactory = injector.getInstance MockFieldComponentFactory
 		preferences = new Preferences()
--- prefdialog-core/src/test/groovy/com/anrisoftware/prefdialog/core/MockModule.groovy
+++ prefdialog-core/src/test/groovy/com/anrisoftware/prefdialog/core/MockModule.groovy
@@ -1,8 +1,31 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of prefdialog-core.
+ * 
+ * prefdialog-core is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * prefdialog-core is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with prefdialog-core. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anrisoftware.prefdialog.core
 
 import com.google.inject.AbstractModule
 import com.google.inject.assistedinject.FactoryModuleBuilder
-
+/**
+ * Binds the abstract field component factory.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
 class MockModule extends AbstractModule {
 
 	@Override
