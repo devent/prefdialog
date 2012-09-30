6555f9dd9b50f41211cea9829466d31f91fec0d3
Erwin Mueller <erwin.mueller@deventm.org>
2012-09-30 23:09:47 +0200

    Rework child panel tests
    #1574

--- prefdialog-corefields/src/test/groovy/com/anrisoftware/prefdialog/fields/child/ChildTest.groovy
+++ prefdialog-corefields/src/test/groovy/com/anrisoftware/prefdialog/fields/child/ChildTest.groovy
@@ -1,18 +1,18 @@
 /*
  * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
- * 
+ *
  * This file is part of prefdialog-corefields.
- * 
+ *
  * prefdialog-corefields is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * prefdialog-corefields is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with prefdialog-corefields. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -32,7 +32,7 @@ import com.google.inject.Injector
 
 /**
  * Test the {@link ChildField}.
- * 
+ *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 2.2
  */
@@ -63,18 +63,24 @@ class ChildTest extends FieldTestUtils {
 
 	@Test
 	void "child null value"() {
-		factory.create(container, bean, CHILD_NULL_VALUE_FIELD).createField()
+		factory.create(container, bean, NULL_VALUE_FIELD).createField()
+		def panel
 		beginPanelFrame title, container, {
-			fixture.panel("$CHILD_NULL_VALUE").requireVisible()
+			panel = fixture.panel(NULL_VALUE)
+			panel.requireVisible()
 		}
 	}
 
 	@Test
 	void "child null value with added field"() {
-		factory.create(container, bean, CHILD_NULL_VALUE_FIELD).createField()
+		factory.create(container, bean, NULL_VALUE_FIELD).createField()
+		def panel
+		def scrollPane
 		beginPanelFrame title, container, {
-			fixture.panel("$CHILD_NULL_VALUE").requireVisible()
-			fixture.scrollPane("$CHILD_NULL_VALUE-$CHILDREN_SCROLL_NAME").requireVisible()
+			panel = fixture.panel(NULL_VALUE)
+			panel.requireVisible()
+			scrollPane = fixture.scrollPane("$NULL_VALUE-$CHILDREN_SCROLL_NAME")
+			scrollPane.requireVisible()
 		}
 	}
 }
