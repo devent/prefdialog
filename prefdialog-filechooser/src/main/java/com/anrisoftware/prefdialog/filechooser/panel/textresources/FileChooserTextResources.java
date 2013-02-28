7996b530d4f5149f1f10d244f5c0f1520447f198
Erwin Mueller <erwin.mueller@deventm.org>
2013-02-28 12:28:13 +0100

    Add action mnemonics

--- prefdialog-filechooser/src/main/java/com/anrisoftware/prefdialog/filechooser/panel/textresources/FileChooserTextResources.java
+++ prefdialog-filechooser/src/main/java/com/anrisoftware/prefdialog/filechooser/panel/textresources/FileChooserTextResources.java
@@ -6,12 +6,14 @@ import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.DEFAULT_ICON_MENU_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.DETAILED_TREE_VIEW_MENU_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.DETAILED_VIEW_MENU_NAME;
+import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.FILTER_LABEL_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.FORWARD_BUTTON_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.HUGE_ICON_MENU_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.ICONS_ONLY_MENU_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.ICON_SIZE_MENU_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.LARGE_ICON_MENU_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.MEDIUM_ICON_MENU_NAME;
+import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.NAME_LABEL_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.OPTIONS_BUTTON_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.PREVIEW_BUTTON_NAME;
 import static com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel.REFRESH_BUTTON_NAME;
@@ -39,6 +41,7 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.swing.AbstractButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 
 import com.anrisoftware.prefdialog.filechooser.panel.api.FileChooserPanel;
 import com.anrisoftware.resources.api.LocaleListener;
@@ -66,7 +69,6 @@ public class FileChooserTextResources implements LocaleListener {
 	}
 
 	private void setupActions() {
-		setActionText(PREVIEW_BUTTON_NAME, "preview");
 		setActionText(PREVIEW_BUTTON_NAME, "preview");
 		setActionText(REFRESH_BUTTON_NAME, "refresh");
 		setActionText(SHOW_PREVIEW_BUTTON_NAME, "show_preview");
@@ -99,17 +101,41 @@ public class FileChooserTextResources implements LocaleListener {
 		setActionText(TEXT_ONLY_MENU_NAME, "text_only");
 		setActionText(ICONS_ONLY_MENU_NAME, "icons_only");
 		setActionText(TEXT_POSITION_MENU_NAME, "text_position");
+		setLabelText(NAME_LABEL_NAME, "name");
+		setLabelText(FILTER_LABEL_NAME, "filter");
 	}
 
 	private void setActionText(String name, String actionName) {
 		AbstractButton button = getFileChooserButton(name);
 		if (button != null) {
 			button.setText(texts.getAction(actionName));
+			button.setMnemonic(texts.getMnemonic(actionName));
+			int index = texts.getMnemonicIndex(actionName);
+			if (index != -1) {
+				button.setDisplayedMnemonicIndex(index);
+			}
 		}
 	}
 
 	private AbstractButton getFileChooserButton(String name) {
 		Map<String, JComponent> components = fileChooser.getComponents();
 		return (AbstractButton) components.get(name);
+	}
+
+	private void setLabelText(String name, String actionName) {
+		JLabel label = getFileChooserLabel(name);
+		if (label != null) {
+			label.setText(texts.getAction(actionName));
+			label.setDisplayedMnemonic(texts.getMnemonic(actionName));
+			int index = texts.getMnemonicIndex(actionName);
+			if (index != -1) {
+				label.setDisplayedMnemonicIndex(index);
+			}
+		}
+	}
+
+	private JLabel getFileChooserLabel(String name) {
+		Map<String, JComponent> components = fileChooser.getComponents();
+		return (JLabel) components.get(name);
 	}
 }
