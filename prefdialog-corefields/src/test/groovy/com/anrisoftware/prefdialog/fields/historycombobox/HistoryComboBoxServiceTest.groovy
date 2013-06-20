453513af868507c0adca23792b10b68ea7108284
Erwin Mueller <erwin.mueller@deventm.org>
2013-06-20 10:34:29 +0200

    Set history combo box field service
    finish #1614

---
+++ prefdialog-corefields/src/test/groovy/com/anrisoftware/prefdialog/fields/historycombobox/HistoryComboBoxServiceTest.groovy
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+ * 
+ * This file is part of prefdialog-corefields.
+ * 
+ * prefdialog-corefields is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * prefdialog-corefields is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with prefdialog-corefields. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.anrisoftware.prefdialog.fields.historycombobox
+
+import static com.anrisoftware.prefdialog.core.AbstractTitleField.*
+import static com.anrisoftware.prefdialog.core.FieldTestUtils.*
+import static com.anrisoftware.prefdialog.fields.historycombobox.HistoryComboBoxBean.*
+import static com.anrisoftware.prefdialog.fields.historycombobox.HistoryComboBoxService.*
+
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+
+import com.anrisoftware.globalpom.utils.TestFrameUtil
+
+/**
+ * @see HistoryComboBox
+ * @see HistoryComboBoxService
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 3.0
+ */
+class HistoryComboBoxServiceTest {
+
+	@Test
+	void "service"() {
+		def title = "$NAME::service"
+		def fieldName = LIST_ELEMENTS
+		def field = factory.create(bean, fieldName)
+		def container = field.getAWTComponent()
+		new TestFrameUtil(title, container).withFixture({})
+	}
+
+	static final String NAME = HistoryComboBoxServiceTest.class.simpleName
+
+	static HistoryComboBoxFactory factory
+
+	HistoryComboBoxBean bean
+
+	@BeforeClass
+	static void setupFactories() {
+		factory = findService(INFO).getFactory()
+	}
+
+	@Before
+	void setupBean() {
+		bean = new HistoryComboBoxBean()
+	}
+}
