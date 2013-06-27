f85e1701cf931ab9953e6933079b466925c01f33
Erwin Mueller <erwin.mueller@deventm.org>
2013-06-27 16:23:06 +0200

    Set preferred size of file text field
    Fiel field is too big if large file path
    fixes #2489

--- prefdialog-misc-swing/src/main/java/com/anrisoftware/prefdialog/miscswing/text/filetext/FileTextField.java
+++ prefdialog-misc-swing/src/main/java/com/anrisoftware/prefdialog/miscswing/text/filetext/FileTextField.java
@@ -1,23 +1,20 @@
 /*
- * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
- * 
- * This file is part of prefdialog-corefields.
- * 
- * prefdialog-corefields is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- * 
- * prefdialog-corefields is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with prefdialog-corefields. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org> This file is part of
+ * prefdialog-corefields. prefdialog-corefields is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version. prefdialog-corefields
+ * is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+ * should have received a copy of the GNU Lesser General Public License along
+ * with prefdialog-corefields. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.prefdialog.miscswing.text.filetext;
 
+import static javax.swing.SwingUtilities.invokeLater;
+
+import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.event.ComponentAdapter;
@@ -27,7 +24,6 @@ import java.awt.event.FocusEvent;
 import java.io.File;
 
 import javax.swing.JFormattedTextField;
-import javax.swing.SwingUtilities;
 import javax.swing.text.DefaultFormatterFactory;
 
 import com.google.inject.Guice;
@@ -72,7 +68,6 @@ public class FileTextField extends JFormattedTextField {
 	 * 
 	 * @param file
 	 *            the initial {@link File}.
-	 * 
 	 * @return the file text field {@link JFormattedTextField}.
 	 */
 	public static JFormattedTextField create(File file) {
@@ -83,42 +78,60 @@ public class FileTextField extends JFormattedTextField {
 
 	private final FileDisplayFormatter displayFormatter;
 
+	private final FocusAdapter focusListener;
+
+	private final ComponentAdapter componentListener;
+
+	private final FileEditFormatter editFormatter;
+
+	private final FileTextTransferHandler fileTextTransferHandler;
+
 	@Inject
 	FileTextField(FileDisplayFormatter displayFormatter,
 			FileEditFormatter editFormatter,
 			FileTextTransferHandler fileTextTransferHandler) {
 		super(new File(""));
 		this.displayFormatter = displayFormatter;
-		setupUpdateOnResizing(displayFormatter);
-		setupCaretToLastCharacterOnFocus();
+		this.editFormatter = editFormatter;
+		this.fileTextTransferHandler = fileTextTransferHandler;
+		this.focusListener = new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				updateCaretToLastCharacterOnFocus();
+			}
+
+		};
+		this.componentListener = new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				updatePathMaxWidth();
+			}
+
+		};
+		setupField();
+	}
+
+	private void setupField() {
+		addFocusListener(focusListener);
+		addComponentListener(componentListener);
 		setFormatterFactory(new DefaultFormatterFactory(displayFormatter,
 				displayFormatter, editFormatter));
 		setTransferHandler(fileTextTransferHandler);
+		setPreferredSize(new Dimension(50, getHeight()));
 	}
 
-	private void setupCaretToLastCharacterOnFocus() {
-		addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				SwingUtilities.invokeLater(new Runnable() {
-
-					@Override
-					public void run() {
-						setCaretToLastCharacter();
-					}
-
-				});
-			}
-		});
+	private void updatePathMaxWidth() {
+		displayFormatter.updatePathMaxWidth(getWidth());
 	}
 
-	private void setupUpdateOnResizing(
-			final FileDisplayFormatter displayFormatter) {
-		addComponentListener(new ComponentAdapter() {
+	private void updateCaretToLastCharacterOnFocus() {
+		invokeLater(new Runnable() {
+
 			@Override
-			public void componentResized(ComponentEvent e) {
-				displayFormatter.updatePathMaxWidth(getWidth());
+			public void run() {
+				setCaretToLastCharacter();
 			}
+
 		});
 	}
 
