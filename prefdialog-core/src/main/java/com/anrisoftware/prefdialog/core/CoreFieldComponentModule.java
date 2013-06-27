79401cae59e756f2293a9e21ada0b8a1d51531dd
Erwin Mueller <erwin.mueller@deventm.org>
2013-06-27 16:45:12 +0200

    Document core module
    #2436

--- prefdialog-core/src/main/java/com/anrisoftware/prefdialog/core/CoreFieldComponentModule.java
+++ prefdialog-core/src/main/java/com/anrisoftware/prefdialog/core/CoreFieldComponentModule.java
@@ -6,6 +6,17 @@ import com.anrisoftware.globalpom.reflection.annotations.AnnotationsModule;
 import com.anrisoftware.globalpom.reflection.beans.BeansModule;
 import com.google.inject.AbstractModule;
 
+/**
+ * Installs the dependent modules for the core fields.
+ * 
+ * @see AnnotationsModule
+ * @see BeansModule
+ * @see AnnotationClassModule
+ * @see MnemonicModule
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
 public class CoreFieldComponentModule extends AbstractModule {
 
 	@Override
