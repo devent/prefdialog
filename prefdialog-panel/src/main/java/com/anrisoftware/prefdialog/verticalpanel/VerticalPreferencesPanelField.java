5adf9cf2540a8e7723bf55b3fe0feb35d9b1dc24
Erwin Mueller <erwin.mueller@deventm.org>
2013-06-27 15:54:37 +0200

    Fixes Deadlock in vertical panel
    Deadlock in vertical panel when using file text field.
    When add field to the panel, the panel tries to get the minimal size.
    The method AbstractDocument#readLock() is called. If the current thread
    is not the AWT thread, then the document is locked.
    fix #2488

--- prefdialog-panel/src/main/java/com/anrisoftware/prefdialog/verticalpanel/VerticalPreferencesPanelField.java
+++ prefdialog-panel/src/main/java/com/anrisoftware/prefdialog/verticalpanel/VerticalPreferencesPanelField.java
@@ -1,20 +1,14 @@
 /*
- * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
- *
- * This file is part of prefdialog-corefields.
- *
- * prefdialog-corefields is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * prefdialog-corefields is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with prefdialog-corefields. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org> This file is part of
+ * prefdialog-corefields. prefdialog-corefields is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version. prefdialog-corefields
+ * is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+ * should have received a copy of the GNU Lesser General Public License along
+ * with prefdialog-corefields. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.prefdialog.verticalpanel;
 
@@ -22,6 +16,7 @@ import static info.clearthought.layout.TableLayoutConstants.FILL;
 import static info.clearthought.layout.TableLayoutConstants.PREFERRED;
 import static java.awt.BorderLayout.CENTER;
 import static java.lang.String.format;
+import static javax.swing.SwingUtilities.invokeLater;
 import info.clearthought.layout.TableLayout;
 
 import java.awt.BorderLayout;
@@ -56,7 +51,6 @@ import com.google.inject.assistedinject.AssistedInject;
  * 
  * @see VerticalPreferencesPanel
  * @see PreferencesPanel
- * 
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 3.0
  */
@@ -181,6 +175,17 @@ public class VerticalPreferencesPanelField extends
 	public void addField(FieldComponent<?> field) {
 		setupFirstField(field);
 		super.addField(field);
+		invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				updateChildrenPanel();
+			}
+
+		});
+	}
+
+	private void updateChildrenPanel() {
 		childenPanel.removeAll();
 		int rowIndex = layout.getNumRow() - 1;
 		insertRow(rowIndex);
