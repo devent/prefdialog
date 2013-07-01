832549f0796c0ec833507905b77a1f456dba9d33
Erwin Mueller <erwin.mueller@deventm.org>
2013-07-01 15:29:29 +0200

    Add javadoc
    #2436

--- prefdialog-csvimportdailog/src/main/java/com/anrisoftware/prefdialog/csvimportdialog/dialog/CsvImportDialogFactory.java
+++ prefdialog-csvimportdailog/src/main/java/com/anrisoftware/prefdialog/csvimportdialog/dialog/CsvImportDialogFactory.java
@@ -4,7 +4,24 @@ import javax.swing.JFrame;
 
 import com.anrisoftware.prefdialog.csvimportdialog.model.CsvImportProperties;
 
+/**
+ * Factory to create a new CSV import dialog.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 3.0
+ */
 public interface CsvImportDialogFactory {
 
+	/**
+	 * Creates the import dialog for the specified frame.
+	 * 
+	 * @param frame
+	 *            the {@link JFrame} for the dialog.
+	 * 
+	 * @param properties
+	 *            the {@link CsvImportProperties}.
+	 * 
+	 * @return the {@link CsvImportDialog}.
+	 */
 	CsvImportDialog create(JFrame frame, CsvImportProperties properties);
 }
