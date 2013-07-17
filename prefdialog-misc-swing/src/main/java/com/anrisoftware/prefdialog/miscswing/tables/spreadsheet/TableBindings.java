904a7c958e29448bae49d8f66892a21d337cfdf1
Erwin Mueller <erwin.mueller@deventm.org>
2013-07-17 11:32:53 +0200

    Fix error if no row or column are selected
    #2444

--- prefdialog-misc-swing/src/main/java/com/anrisoftware/prefdialog/miscswing/tables/spreadsheet/TableBindings.java
+++ prefdialog-misc-swing/src/main/java/com/anrisoftware/prefdialog/miscswing/tables/spreadsheet/TableBindings.java
@@ -1,18 +1,18 @@
 /*
  * Copyright 2013-2013 Erwin Müller <erwin.mueller@deventm.org>
- *
+ * 
  * This file is part of prefdialog-misc-swing.
- *
- * prefdialog-misc-swing is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
+ * 
+ * prefdialog-misc-swing is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- *
+ * 
  * prefdialog-misc-swing is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with prefdialog-misc-swing. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -58,6 +58,9 @@ class TableBindings {
 					public void valueChanged(ListSelectionEvent e) {
 						int rowIndex = table.getSelectedRow();
 						int index = table.getSelectedColumn();
+						if (rowIndex < 0 || index < 0) {
+							return;
+						}
 						TableModel model = table.getModel();
 						while (!model.isCellEditable(rowIndex, index)) {
 							index++;
