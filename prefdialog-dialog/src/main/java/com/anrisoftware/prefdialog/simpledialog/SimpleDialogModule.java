625dd61893eb016d9da686516e8f597e729c5558
Erwin Mueller <erwin.mueller@deventm.org>
2013-08-05 13:48:27 +0200

    Add dialog sub-module
    - #2546

--- prefdialog-dialog/src/main/java/com/anrisoftware/prefdialog/simpledialog/SimpleDialogModule.java
+++ prefdialog-dialog/src/main/java/com/anrisoftware/prefdialog/simpledialog/SimpleDialogModule.java
@@ -1,21 +1,20 @@
 /*
- * Copyright 2013-2013 Erwin Müller <erwin.mueller@deventm.org>
- * 
- * This file is part of prefdialog-csvimportdialog.
- * 
- * prefdialog-csvimportdialog is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- * 
- * prefdialog-csvimportdialog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
- * 
+ * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of prefdialog-dialog.
+ *
+ * prefdialog-dialog is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * prefdialog-dialog is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
- * along with prefdialog-csvimportdialog. If not, see
- * <http://www.gnu.org/licenses/>.
+ * along with prefdialog-dialog. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.prefdialog.simpledialog;
 
