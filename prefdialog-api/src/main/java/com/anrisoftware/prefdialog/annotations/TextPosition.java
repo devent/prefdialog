aa373c842fd66df1b34f1d5a15dcb71273d28a57
Erwin Mueller <erwin.mueller@deventm.org>
2013-08-05 13:46:54 +0200

    Update lincese headers
    #2546

--- prefdialog-api/src/main/java/com/anrisoftware/prefdialog/annotations/TextPosition.java
+++ prefdialog-api/src/main/java/com/anrisoftware/prefdialog/annotations/TextPosition.java
@@ -1,18 +1,18 @@
 /*
  * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
- * 
+ *
  * This file is part of prefdialog-api.
- * 
+ *
  * prefdialog-api is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
- * prefdialog-api is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *
+ * prefdialog-api is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with prefdialog-api. If not, see <http://www.gnu.org/licenses/>.
  */
