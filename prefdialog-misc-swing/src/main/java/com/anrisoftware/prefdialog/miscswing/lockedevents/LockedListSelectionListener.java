34313536523db7602214e0a558185cf8441673f7
Erwin Mueller <erwin.mueller@deventm.org>
2013-08-21 13:49:46 +0200

    Add skip value adjusting
    #2444

--- prefdialog-misc-swing/src/main/java/com/anrisoftware/prefdialog/miscswing/lockedevents/LockedListSelectionListener.java
+++ prefdialog-misc-swing/src/main/java/com/anrisoftware/prefdialog/miscswing/lockedevents/LockedListSelectionListener.java
@@ -1,18 +1,18 @@
 /*
  * Copyright 2013-2013 Erwin Müller <erwin.mueller@deventm.org>
- *
+ * 
  * This file is part of prefdialog-misc-swing.
- *
- * prefdialog-misc-swing is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
+ * 
+ * prefdialog-misc-swing is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- *
+ * 
  * prefdialog-misc-swing is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with prefdialog-misc-swing. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -39,7 +39,16 @@ public class LockedListSelectionListener extends AbstractLockable implements
 	}
 
 	/**
-	 * Decorate the specified listener with locking ability.
+	 * @see #decorate(boolean, ListSelectionListener)
+	 */
+	public static LockedListSelectionListener lockedListSelectionListener(
+			boolean skipAdjusting, ListSelectionListener l) {
+		return decorate(skipAdjusting, l);
+	}
+
+	/**
+	 * Decorate the specified listener with locking ability and skip value
+	 * adjusting events.
 	 * 
 	 * @param l
 	 *            the {@link ListSelectionListener}.
@@ -47,19 +56,40 @@ public class LockedListSelectionListener extends AbstractLockable implements
 	 * @return the {@link LockedListSelectionListener}.
 	 */
 	public static LockedListSelectionListener decorate(ListSelectionListener l) {
-		return new LockedListSelectionListener(l);
+		return new LockedListSelectionListener(true, l);
+	}
+
+	/**
+	 * Decorate the specified listener with locking ability.
+	 * 
+	 * @param skipAdjusting
+	 *            set to {@code true} to skip value adjusting events.
+	 * 
+	 * @param l
+	 *            the {@link ListSelectionListener}.
+	 * 
+	 * @return the {@link LockedListSelectionListener}.
+	 */
+	public static LockedListSelectionListener decorate(boolean skipAdjusting,
+			ListSelectionListener l) {
+		return new LockedListSelectionListener(skipAdjusting, l);
 	}
 
 	private final ListSelectionListener l;
 
-	LockedListSelectionListener(ListSelectionListener l) {
+	private final boolean skipAdjusting;
+
+	LockedListSelectionListener(boolean skipAdjusting, ListSelectionListener l) {
+		this.skipAdjusting = skipAdjusting;
 		this.l = l;
 	}
 
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		if (!isLock()) {
-			l.valueChanged(e);
+			if (skipAdjusting && (!skipAdjusting || !e.getValueIsAdjusting())) {
+				l.valueChanged(e);
+			}
 		}
 	}
 }
