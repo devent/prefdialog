9d4d4a5b3caeb5aa7b0a8f983b9ac71a4a7d3a6d
Erwin Mueller <erwin.mueller@deventm.org>
2013-10-09 21:34:16 +0200

    Remove CSV/dialog from logging messages

--- prefdialog-dialog/src/main/java/com/anrisoftware/prefdialog/simpledialog/AbstractDialogActionLogger.java
+++ prefdialog-dialog/src/main/java/com/anrisoftware/prefdialog/simpledialog/AbstractDialogActionLogger.java
@@ -1,18 +1,18 @@
 /*
  * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
- *
+ * 
  * This file is part of prefdialog-dialog.
- *
+ * 
  * prefdialog-dialog is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- *
+ * 
  * prefdialog-dialog is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with prefdialog-dialog. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -42,8 +42,8 @@ class AbstractDialogActionLogger extends AbstractLogger {
 	private static final String NO_DIALOG = "No dialog set for {}.";
 	private static final String NO_THREADS = "No threads set for {}.";
 	private static final String NO_FRAME = "No parent frame set for {}.";
-	private static final String ERROR_DIALOG = "Error while creating CSV import dialog: {}.";
-	private static final String INTERRUPTED_DIALOG = "Interrupted while creating CSV import dialog.";
+	private static final String ERROR_DIALOG = "Error while creating dialog: {}.";
+	private static final String INTERRUPTED_DIALOG = "Interrupted while creating dialog.";
 
 	/**
 	 * Creates a logger for {@link AbstractDialogAction}.
