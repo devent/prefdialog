13470689bf720e07502ed05636b39ec6f47eec02
Erwin Mueller <erwin.mueller@deventm.org>
2014-03-19 15:54:14 +0100

    Switch off restore input for button group.

--- prefdialog-corefields/src/main/java/com/anrisoftware/prefdialog/fields/buttongroup/ButtonGroupField.java
+++ prefdialog-corefields/src/main/java/com/anrisoftware/prefdialog/fields/buttongroup/ButtonGroupField.java
@@ -47,115 +47,123 @@ import com.google.inject.assistedinject.Assisted;
 @SuppressWarnings("serial")
 public class ButtonGroupField extends AbstractTitleField<JPanel> {
 
-	private static final String HORIZONTAL_ALIGNMENT_ELEMENT = "horizontalAlignment";
+    private static final String HORIZONTAL_ALIGNMENT_ELEMENT = "horizontalAlignment";
 
-	private static final Class<? extends Annotation> ANNOTATION_CLASS = ButtonGroup.class;
+    private static final Class<? extends Annotation> ANNOTATION_CLASS = ButtonGroup.class;
 
-	private final ButtonGroupFieldLogger log;
+    private final ButtonGroupFieldLogger log;
 
-	private final ButtonsGroupPanel buttonsGroupPanel;
+    private final ButtonsGroupPanel buttonsGroupPanel;
 
-	private final ButtonsRowPanel buttonsRowPanel;
+    private final ButtonsRowPanel buttonsRowPanel;
 
-	private AnnotationAccess fieldAnnotation;
+    private AnnotationAccess fieldAnnotation;
 
-	/**
-	 * @see ButtonGroupFieldFactory#create(Object, String)
-	 */
-	@Inject
-	ButtonGroupField(ButtonGroupFieldLogger logger,
-			ButtonsGroupPanel buttonsGroupPanel,
-			ButtonsRowPanel buttonsRowPanel, @Assisted Object parentObject,
-			@Assisted String fieldName) {
-		super(buttonsGroupPanel, parentObject, fieldName);
-		this.log = logger;
-		this.buttonsGroupPanel = buttonsGroupPanel;
-		this.buttonsRowPanel = buttonsRowPanel;
-		setup();
-	}
+    /**
+     * @see ButtonGroupFieldFactory#create(Object, String)
+     */
+    @Inject
+    ButtonGroupField(ButtonGroupFieldLogger logger,
+            ButtonsGroupPanel buttonsGroupPanel,
+            ButtonsRowPanel buttonsRowPanel, @Assisted Object parentObject,
+            @Assisted String fieldName) {
+        super(buttonsGroupPanel, parentObject, fieldName);
+        this.log = logger;
+        this.buttonsGroupPanel = buttonsGroupPanel;
+        this.buttonsRowPanel = buttonsRowPanel;
+        setup();
+    }
 
-	private void setup() {
-		buttonsGroupPanel.setButtonsRowPanel(buttonsRowPanel);
-	}
+    private void setup() {
+        buttonsGroupPanel.setButtonsRowPanel(buttonsRowPanel);
+    }
 
-	@Inject
-	void setBeanAccessFactory(AnnotationAccessFactory annotationAccessFactory) {
-		this.fieldAnnotation = annotationAccessFactory.create(ANNOTATION_CLASS,
-				getAccessibleObject());
-		setupHorizontalAlignment();
-	}
+    @Inject
+    void setBeanAccessFactory(AnnotationAccessFactory annotationAccessFactory) {
+        this.fieldAnnotation = annotationAccessFactory.create(ANNOTATION_CLASS,
+                getAccessibleObject());
+        setupHorizontalAlignment();
+    }
 
-	private void setupHorizontalAlignment() {
-		HorizontalAlignment alignment = fieldAnnotation
-				.getValue(HORIZONTAL_ALIGNMENT_ELEMENT);
-		setHorizontalAlignment(alignment);
-	}
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+        buttonsGroupPanel.setName(name);
+        buttonsRowPanel.setName(name);
+    }
 
-	@Override
-	public void setName(String name) {
-		super.setName(name);
-		buttonsGroupPanel.setName(name);
-		buttonsRowPanel.setName(name);
-	}
+    @Override
+    public void setValue(Object value) throws PropertyVetoException {
+        super.setValue(value);
+        setupButtons();
+    }
 
-	@Override
-	public void setValue(Object value) throws PropertyVetoException {
-		super.setValue(value);
-		List<Action> actions = asList(getValue());
-		AbstractListModel<Action> model = createModel(actions);
-		buttonsRowPanel.setModel(model);
-	}
+    @Override
+    public void restoreInput() throws PropertyVetoException {
+    }
 
-	private AbstractListModel<Action> createModel(final List<Action> actions) {
-		return new AbstractListModel<Action>() {
+    /**
+     * Sets the horizontal alignment of the button group.
+     * 
+     * @param alignment
+     *            the {@link HorizontalAlignment}.
+     */
+    public void setHorizontalAlignment(HorizontalAlignment alignment) {
+        buttonsGroupPanel.setHorizontalAlignment(alignment);
+        log.horizontalAlignmentSet(this, alignment);
+    }
 
-			@Override
-			public int getSize() {
-				return actions.size();
-			}
+    /**
+     * Returns the horizontal alignment of the button group.
+     * 
+     * @return the {@link HorizontalAlignment}.
+     */
+    public HorizontalAlignment getHorizontalAlignment() {
+        return buttonsGroupPanel.getHorizontalAlignment();
+    }
 
-			@Override
-			public Action getElementAt(int i) {
-				return actions.get(i);
-			}
-		};
-	}
+    private void setupHorizontalAlignment() {
+        HorizontalAlignment alignment = fieldAnnotation
+                .getValue(HORIZONTAL_ALIGNMENT_ELEMENT);
+        setHorizontalAlignment(alignment);
+    }
 
-	@SuppressWarnings("unchecked")
-	private List<Action> asList(Object value) {
-		if (ArrayUtils.isSameType(value, new Action[0])) {
-			return Arrays.asList((Action[]) value);
-		} else if (value instanceof Iterable) {
-			return createList((Iterable<? extends Action>) value);
-		}
-		throw log.valueNotList(value);
-	}
+    private void setupButtons() {
+        List<Action> actions = asList(getValue());
+        AbstractListModel<Action> model = createModel(actions);
+        buttonsRowPanel.setModel(model);
+    }
 
-	private List<Action> createList(Iterable<? extends Action> it) {
-		List<Action> list = new ArrayList<Action>();
-		for (Action action : it) {
-			list.add(action);
-		}
-		return list;
-	}
+    private AbstractListModel<Action> createModel(final List<Action> actions) {
+        return new AbstractListModel<Action>() {
 
-	/**
-	 * Sets the horizontal alignment of the button group.
-	 * 
-	 * @param alignment
-	 *            the {@link HorizontalAlignment}.
-	 */
-	public void setHorizontalAlignment(HorizontalAlignment alignment) {
-		buttonsGroupPanel.setHorizontalAlignment(alignment);
-		log.horizontalAlignmentSet(this, alignment);
-	}
+            @Override
+            public int getSize() {
+                return actions.size();
+            }
 
-	/**
-	 * Returns the horizontal alignment of the button group.
-	 * 
-	 * @return the {@link HorizontalAlignment}.
-	 */
-	public HorizontalAlignment getHorizontalAlignment() {
-		return buttonsGroupPanel.getHorizontalAlignment();
-	}
+            @Override
+            public Action getElementAt(int i) {
+                return actions.get(i);
+            }
+        };
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Action> asList(Object value) {
+        if (ArrayUtils.isSameType(value, new Action[0])) {
+            return Arrays.asList((Action[]) value);
+        } else if (value instanceof Iterable) {
+            return createList((Iterable<? extends Action>) value);
+        }
+        throw log.valueNotList(value);
+    }
+
+    private List<Action> createList(Iterable<? extends Action> it) {
+        List<Action> list = new ArrayList<Action>();
+        for (Action action : it) {
+            list.add(action);
+        }
+        return list;
+    }
 }
