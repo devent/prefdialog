8b57383d36ef94dc39ddbd9ee9606fd1a77626aa
Erwin Mueller <erwin.mueller@deventm.org>
2015-10-14 17:59:58 +0200

    Release version 3.3
    finish #3431

--- prefdialog-appdialogs/src/main/java/com/anrisoftware/prefdialog/appdialogs/aboutdialog/AboutDialogCreateWorker.java
+++ prefdialog-appdialogs/src/main/java/com/anrisoftware/prefdialog/appdialogs/aboutdialog/AboutDialogCreateWorker.java
@@ -1,20 +1,20 @@
 /*
- * Copyright 2013-2015 Erwin Müller <erwin.mueller@deventm.org>
+ * Copyright 2015 Erwin Müller <erwin.mueller@deventm.org>
  *
- * This file is part of prefdialog-misc-swing.
+ * This file is part of prefdialog-appdialogs.
  *
- * prefdialog-misc-swing is free software: you can redistribute it and/or modify it
+ * prefdialog-appdialogs is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * prefdialog-misc-swing is distributed in the hope that it will be useful, but
+ * prefdialog-appdialogs is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with prefdialog-misc-swing. If not, see <http://www.gnu.org/licenses/>.
+ * along with prefdialog-appdialogs. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.prefdialog.appdialogs.aboutdialog;
 
