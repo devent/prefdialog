48c8d15c5e1a12e1d60b638923c9ce49c8b84d51
Erwin Mueller <erwin.mueller@deventm.org>
2016-01-15 15:03:19 +0100

    Fix error getInjector() not found
    see #3476

--- prefdialog-misc-swing/src/test/groovy/com/anrisoftware/prefdialog/miscswing/comboboxhistory/ItemDefaultTest.groovy
+++ prefdialog-misc-swing/src/test/groovy/com/anrisoftware/prefdialog/miscswing/comboboxhistory/ItemDefaultTest.groovy
@@ -18,26 +18,26 @@
  */
 package com.anrisoftware.prefdialog.miscswing.comboboxhistory
 
-import static com.anrisoftware.prefdialog.miscswing.comboboxhistory.ComboBoxHistoryModule.*
+import static com.anrisoftware.prefdialog.miscswing.comboboxhistory.ComboBoxHistoryModule.InstanceHolder.*
 
 import org.junit.Test
 
 /**
  * @see ItemDefault
- * 
+ *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.0
  */
 class ItemDefaultTest {
 
-	@Test
-	void "equals"() {
-		String item = "default A"
-		def defitem = getInjector().getInstance(ItemDefaultFactory).create(item)
-		assert item.hashCode() == defitem.hashCode()
-		assert defitem.equals(item)
-		assert !item.equals(defitem)
-		assert defitem == item
-		assert item != defitem
-	}
+    @Test
+    void "equals"() {
+        String item = "default A"
+        def defitem = injector.getInstance(ItemDefaultFactory).create(item)
+        assert item.hashCode() == defitem.hashCode()
+        assert defitem.equals(item)
+        assert !item.equals(defitem)
+        assert defitem == item
+        assert item != defitem
+    }
 }
